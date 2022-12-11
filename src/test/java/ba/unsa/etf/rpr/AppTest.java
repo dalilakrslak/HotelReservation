7eb5f91e6bb2f55b78885ad05995a478ab8924e9
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-11 15:36:59 +0100

    created project

---
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
