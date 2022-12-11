e76be71eab276d838513eb2522f0f18aef289140
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-11 16:01:37 +0100

    Added ReservationsDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationsDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Reservations;
+
+/**
+ * Dao interface for Reservations domain bean
+ *
+ * @author Dalila Krslak
+ */
+public interface ReservationsDao extends Dao<Reservations>{
+}
