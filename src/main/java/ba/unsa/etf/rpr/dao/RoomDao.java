a20537bda963ca054a980ef128b6ac9836eba18a
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-11 16:00:08 +0100

    Added RoomDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/RoomDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Room;
+
+/**
+ * Dao interface for Room domain bean
+ *
+ * @author Dalila Krslak
+ */
+public interface RoomDao extends Dao<Room>{
+}
