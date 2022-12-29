53f87eddcbf42e39e9f6ba9e6629db48187dae37
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-29 14:09:39 +0100

    added adminOnAction function in FirstPageController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/FirstPageController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/FirstPageController.java
@@ -1,9 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class FirstPageController {
     public Button adminId;
     public Button guestId;
 
+    public void adminOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/adminLogInForm.fxml"));
+        AdminLogInFormController controller = new AdminLogInFormController();
+        loader.setController(controller);
+        stage.setTitle("Hotel Reservation Admin Log In");
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
 }
