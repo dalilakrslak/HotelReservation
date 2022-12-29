7c45dfa945f58e00cb228f7c43f0e1639aa977cb
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-29 14:40:32 +0100

    added attributes in HomeController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,6 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class HomeController {
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 
+public class HomeController {
+    public Button homeButtonId;
+    public Button aboutUsButtonId;
+    public Button roomsButtonId;
+    public Button contactButton;
+    public Button bookButtonId;
+    public BorderPane homePaneId;
 
 }
