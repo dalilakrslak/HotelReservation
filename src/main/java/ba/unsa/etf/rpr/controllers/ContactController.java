d8706b2e5f69eed16e320900c072c15e5d093107
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-29 14:53:09 +0100

    created caboutusOnAction function in ContactController.java

--- src/main/java/ba/unsa/etf/rpr/controllers/ContactController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ContactController.java
@@ -29,4 +29,18 @@ public class ContactController {
         contactStage.hide();
         stage.show();
     }
+
+    public void caboutusOnAction(ActionEvent actionEvent) throws IOException {
+        final Stage  contactStage = (Stage) contactPaneId.getScene().getWindow();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/aboutUs.fxml"));
+        AboutUsController controller = new AboutUsController();
+        loader.setController(controller);
+        stage.setTitle("Hotel About Us");
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        contactStage.hide();
+        stage.show();
+    }
+
 }
