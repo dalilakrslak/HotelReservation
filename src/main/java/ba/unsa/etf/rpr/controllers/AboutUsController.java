877c466ac1250943474bea3bb19f6805e705cdc0
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-29 14:46:30 +0100

    added homeOnAction in AboutUsController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
@@ -1,12 +1,31 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class AboutUsController {
     public Button homeButtonId;
     public Button aboutusButtonId;
     public Button roomsButtonId;
     public Button contactButtonOnAction;
     public BorderPane aboutusPaneId;
+    public void homeOnAction(ActionEvent actionEvent) throws IOException {
+        final Stage aboutusStage = (Stage) aboutusPaneId.getScene().getWindow();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
+        HomeController controller = new HomeController();
+        loader.setController(controller);
+        stage.setTitle("Hotel");
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        aboutusStage.hide();
+        stage.show();
+    }
 }
