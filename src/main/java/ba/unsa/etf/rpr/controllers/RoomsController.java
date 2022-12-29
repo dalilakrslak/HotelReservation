b47d6cc66446b05f987a64428234ae6625aa77d4
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-29 14:57:12 +0100

    added homeOnAction function in RoomsController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/RoomsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RoomsController.java
@@ -1,12 +1,31 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class RoomsController {
     public Button homeButtonId;
     public Button aboutusButtonId;
     public Button roomsButtonId;
     public Button contactButtonId;
     public BorderPane roomsPaneId;
+    public void homeOnAction(ActionEvent actionEvent) throws IOException {
+        final Stage roomsStage = (Stage) roomsPaneId.getScene().getWindow();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
+        HomeController controller = new HomeController();
+        loader.setController(controller);
+        stage.setTitle("Hotel");
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        roomsStage.hide();
+        stage.show();
+    }
 }
