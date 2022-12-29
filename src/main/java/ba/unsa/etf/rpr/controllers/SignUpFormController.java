847acd26688e74d86af9375e39c4285cc38d56f3
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-29 14:33:46 +0100

    added initialize function in SignUpFormController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/SignUpFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignUpFormController.java
@@ -1,9 +1,19 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class SignUpFormController {
     public TextField usernameId;
     public Text invalidUsernameId;
+    @FXML
+    public void initialize(){
+        usernameId.textProperty().addListener((obs, oldValue, newValue)->{
+            if(newValue.length()>=5)
+                invalidUsernameId.setText("");
+            else
+                invalidUsernameId.setText("Username must have atleast 5 characters!");
+        });
+    }
 }
