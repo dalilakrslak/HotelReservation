5daa466c2d9f526b570b665a6222fdd6ab75af1e
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-29 14:23:52 +0100

    added attributes in LogInFormController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/LogInFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LogInFormController.java
@@ -1,5 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class LogInFormController {
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
+public class LogInFormController {
+    public TextField usernameId;
+    public PasswordField passwordId;
+    public Button loginButtonId;
+    public Hyperlink registerId;
+    public Button cancelButtonId;
+    public GridPane loginFormPaneId;
 }
