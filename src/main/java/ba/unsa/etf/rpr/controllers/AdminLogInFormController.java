e1b744ad7c31f794ec1ce4b55e822320bf6ea3f4
dalilakrslak <dkrslak1@etf.unsa.ba>
2022-12-29 14:15:00 +0100

    added attributes in AdminLogInFormController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminLogInFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminLogInFormController.java
@@ -1,5 +1,12 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Button;
 public class AdminLogInFormController {
+    public TextField usernameId;
+    public PasswordField passwordId;
+    public Button loginButtonId;
+    public Button cancelButtonId;
 
 }
