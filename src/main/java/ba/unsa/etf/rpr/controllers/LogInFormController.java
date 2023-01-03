d2c4158295cf2a00825ef96e4515f8b650f410ac
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-03 13:16:46 +0100

    clean up of LogInFormController.java.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/LogInFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LogInFormController.java
@@ -2,8 +2,6 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
@@ -13,7 +11,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class LogInFormController {
     public TextField usernameId;
     public PasswordField passwordId;
