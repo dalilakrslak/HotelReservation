1870beb7b5d233dbab993b3307da7cc6586ef0fd
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-03 12:53:02 +0100

    added function openWindon in OpenNewStage.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/OpenNewStage.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/OpenNewStage.java
@@ -1,5 +1,25 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class OpenNewStage {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+public class OpenNewStage {
+    public <Tip extends Pane> void openWindow(Tip pane, String name) throws IOException {
+        final Stage homeStage = (Stage) pane.getScene().getWindow();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/"+ name +".fxml"));
+        ContactController controller = new ContactController();
+        loader.setController(controller);
+        stage.setTitle("DK Hotel");
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        homeStage.hide();
+        stage.show();
+    }
 }
