c16810e4ccbb7a5112f88498b95be52f951430c2
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-03 13:16:07 +0100

    clean up of AboutUsController.java class

--- src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
@@ -1,15 +1,11 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class AboutUsController {
     public Button homeButtonId;
     public Button aboutusButtonId;
