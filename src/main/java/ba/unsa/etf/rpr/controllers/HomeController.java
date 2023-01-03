f4276b4add7ebed4e144fa5765ffe294cadf381c
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-03 12:56:42 +0100

    clean up of code in contactOnAction function

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -53,16 +53,8 @@ public class HomeController {
         stage.show();
     }
     public void contactOnAction(ActionEvent actionEvent) throws IOException {
-        final Stage  homeStage = (Stage) homePaneId.getScene().getWindow();
-        Stage stage = new Stage();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/contact.fxml"));
-        ContactController controller = new ContactController();
-        loader.setController(controller);
-        stage.setTitle("Hotel Contact");
-        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        homeStage.hide();
-        stage.show();
+        OpenNewStage o = new OpenNewStage();
+        o.openWindow(homePaneId, "contact");
     }
     public void bookNowOnAction(ActionEvent actionEvent) throws IOException {
         OpenNewStage o = new OpenNewStage();
