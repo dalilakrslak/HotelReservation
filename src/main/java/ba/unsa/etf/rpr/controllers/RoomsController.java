73bad16a0739edefb678468f44f75cdd3412927b
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-03 13:12:51 +0100

    clean up of code in aboutusOnAction function

--- src/main/java/ba/unsa/etf/rpr/controllers/RoomsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RoomsController.java
@@ -22,16 +22,8 @@ public class RoomsController {
     }
 
     public void aboutusOnAction(ActionEvent actionEvent) throws IOException {
-        final Stage  roomsStage = (Stage) roomsPaneId.getScene().getWindow();
-        Stage stage = new Stage();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/aboutUs.fxml"));
-        AboutUsController controller = new AboutUsController();
-        loader.setController(controller);
-        stage.setTitle("Hotel About Us");
-        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        roomsStage.hide();
-        stage.show();
+        OpenNewStage o = new OpenNewStage();
+        o.openWindow(roomsPaneId, "aboutUs");
     }
 
     public void roomsOnAction(ActionEvent actionEvent) throws IOException {
