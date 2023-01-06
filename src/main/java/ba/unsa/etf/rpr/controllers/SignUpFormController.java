34abdc99e12bfe3f094a48167388be9f32ce8f1a
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-06 14:24:42 +0100

    small changes in SignUpFormController.java

--- src/main/java/ba/unsa/etf/rpr/controllers/SignUpFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignUpFormController.java
@@ -1,14 +1,24 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class SignUpFormController {
     public TextField usernameId;
+    public TextField firstNameId;
+    public TextField lastNameId;
+    public TextField emailId;
     public Text invalidUsernameId;
+    public PasswordField passwordId;
     @FXML
     public void initialize(){
+        firstNameId.setFocusTraversable(false);
+        lastNameId.setFocusTraversable(false);
+        emailId.setFocusTraversable(false);
+        usernameId.setFocusTraversable(false);
+        passwordId.setFocusTraversable(false);
         usernameId.textProperty().addListener((obs, oldValue, newValue)->{
             if(newValue.length()>=5)
                 invalidUsernameId.setText("");
