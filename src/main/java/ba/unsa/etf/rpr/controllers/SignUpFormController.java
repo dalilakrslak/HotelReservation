4602ee6d9a3b3a7d02382bbf803de8169ef2e490
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-06 18:57:08 +0100

    added availability of username

--- src/main/java/ba/unsa/etf/rpr/controllers/SignUpFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignUpFormController.java
@@ -1,17 +1,33 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.HotelException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static ba.unsa.etf.rpr.dao.AbstractDao.getConnection;
 
 public class SignUpFormController {
     public TextField usernameId;
     public TextField firstNameId;
     public TextField lastNameId;
     public TextField emailId;
+    public Text emptyName;
     public Text invalidUsernameId;
+    public Text emptyEmail;
+    public Text emptyPassword;
     public PasswordField passwordId;
+    public Button signupId;
     @FXML
     public void initialize(){
         firstNameId.setFocusTraversable(false);
@@ -25,5 +41,33 @@ public class SignUpFormController {
             else
                 invalidUsernameId.setText("Username must have atleast 5 characters!");
         });
+
+    }
+
+    public void signupButtonOnAction(ActionEvent actionEvent) throws HotelException {
+        if (usernameId.getText().isBlank() == false && passwordId.getText().isBlank() == false && firstNameId.getText().isBlank()==false && lastNameId.getText().isBlank()==false && emailId.getText().isBlank() == false) {
+            String username = usernameId.getText();
+            UserDaoSQLImpl u = new UserDaoSQLImpl();
+            User user = new User();
+            boolean flag = checkUsername(username);
+            if (flag) {
+                invalidUsernameId.setText("Username already exists!");
+
+            }
+        }
+    }
+    public boolean checkUsername(String username) {
+        String sql = "SELECT * FROM USER WHERE username = ?";
+        try {
+            PreparedStatement s=getConnection().prepareStatement(sql);
+            s.setString(1, username);
+            ResultSet r = s.executeQuery();
+            while(r.next()){
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
