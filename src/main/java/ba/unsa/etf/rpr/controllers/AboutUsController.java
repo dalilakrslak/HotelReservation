7a5ed928836e798f6fef988c214b877b0b4b4b98
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-06 14:13:14 +0100

    clean up of code in AboutUsController.java

--- src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AboutUsController.java
@@ -1,10 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class AboutUsController {
     public Button homeButtonId;
@@ -13,19 +18,27 @@ public class AboutUsController {
     public Button contactButtonOnAction;
     public BorderPane aboutusPaneId;
     public void homeOnAction(ActionEvent actionEvent) throws IOException {
-        OpenNewStage o = new OpenNewStage();
-        o.openWindow(aboutusPaneId, "home");
+        openDialog("Home", "/fxml/home.fxml", new HomeController());
     }
     public void aboutusOnAction(ActionEvent actionEvent) throws IOException {
-        OpenNewStage o = new OpenNewStage();
-        o.openWindow(aboutusPaneId, "aboutUs");
+        openDialog("About us", "/fxml/aboutUs.fxml", new AboutUsController());
     }
     public void roomsOnAction(ActionEvent actionEvent) throws IOException {
-        OpenNewStage o = new OpenNewStage();
-        o.openWindow(aboutusPaneId, "rooms");
+        openDialog("Rooms", "/fxml/rooms.fxml", new RoomsController());
     }
     public void contactOnAction(ActionEvent actionEvent) throws IOException {
-        OpenNewStage o = new OpenNewStage();
-        o.openWindow(aboutusPaneId, "contact");
+        openDialog("Contact", "/fxml/contact.fxml", new ContactController());
     }
+    private void openDialog(String title, String file, Object controller) throws IOException {
+        final Stage homeStage = (Stage) aboutusPaneId.getScene().getWindow();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+        loader.setController(controller);
+        stage.setTitle(title);
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        homeStage.hide();
+        stage.show();
+    }
+
 }
