fe1d508ed9a4f2002873347f550009d87ea8848c
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-06 14:14:15 +0100

    clean up of code in HomeController.java

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,10 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class HomeController {
     public Button homeButtonId;
@@ -14,23 +19,29 @@ public class HomeController {
     public Button bookButtonId;
     public BorderPane homePaneId;
     public void homeOnAction(ActionEvent actionEvent) throws IOException {
-        OpenNewStage o = new OpenNewStage();
-        o.openWindow(homePaneId, "home");
+        openDialog("Home", "/fxml/home.fxml", new HomeController());
     }
     public void aboutUsOnAction(ActionEvent actionEvent) throws IOException {
-        OpenNewStage o = new OpenNewStage();
-        o.openWindow(homePaneId, "aboutUs");
+        openDialog("About us", "/fxml/aboutUs.fxml", new AboutUsController());
     }
     public void roomsOnAction(ActionEvent actionEvent) throws IOException {
-        OpenNewStage o = new OpenNewStage();
-        o.openWindow(homePaneId, "rooms");
+        openDialog("Rooms", "/fxml/rooms.fxml", new RoomsController());
     }
     public void contactOnAction(ActionEvent actionEvent) throws IOException {
-        OpenNewStage o = new OpenNewStage();
-        o.openWindow(homePaneId, "contact");
+        openDialog("Contact", "/fxml/contact.fxml", new ContactController());
     }
     public void bookNowOnAction(ActionEvent actionEvent) throws IOException {
-        OpenNewStage o = new OpenNewStage();
-        o.openWindow(homePaneId, "booking");
+        openDialog("Booking", "/fxml/booking.fxml", new BookingController());
+    }
+    private void openDialog(String title, String file, Object controller) throws IOException {
+        final Stage homeStage = (Stage) homePaneId.getScene().getWindow();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+        loader.setController(controller);
+        stage.setTitle(title);
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        homeStage.hide();
+        stage.show();
     }
 }
