94fc49ac3fb8c3ad1595e4edfea1bf5afc1dde77
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-07 00:43:54 +0100

    changes in BookingController.java

--- src/main/java/ba/unsa/etf/rpr/controllers/BookingController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/BookingController.java
@@ -1,5 +1,25 @@
 package ba.unsa.etf.rpr.controllers;
 
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+import java.util.ArrayList;
+
 public class BookingController {
+    public ChoiceBox<String> roomId;
+    private ArrayList<String> rooms = new ArrayList<>();
+
+    public BookingController(){
+        rooms.add("Single bed");
+        rooms.add("Room for couples");
+        rooms.add("Two bed");
+        rooms.add("Family room");
+    }
+    @FXML
+    public void initialize(){
+        roomId.setItems(FXCollections.observableList(rooms));
+    }
 
 }
