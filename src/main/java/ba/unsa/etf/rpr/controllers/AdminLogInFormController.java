45ef7bcefc054dc1b6dc76aee743ef3167ba027d
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-07 14:03:22 +0100

    added openDialog method in AdminLogInFormController.java

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminLogInFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminLogInFormController.java
@@ -2,16 +2,24 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class AdminLogInFormController {
     public TextField usernameId;
     public PasswordField passwordId;
     public Button loginButtonId;
     public Button cancelButtonId;
+    public GridPane adminPane;
 
     public void loginOnAction(ActionEvent actionEvent) {
     }
@@ -25,5 +33,17 @@ public class AdminLogInFormController {
     public void initialize(){
         usernameId.setFocusTraversable(false);
         passwordId.setFocusTraversable(false);
+    }
+
+    private void openDialog(String title, String file, Object controller) throws IOException {
+        final Stage homeStage = (Stage) adminPane.getScene().getWindow();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+        loader.setController(controller);
+        stage.setTitle(title);
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        homeStage.hide();
+        stage.show();
     }
 }
