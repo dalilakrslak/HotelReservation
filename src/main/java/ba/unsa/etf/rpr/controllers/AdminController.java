dd3fe5001225eae35f01b3ac718e568fa146cf84
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-01-10 08:14:03 +0100

    added methods for User table

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
@@ -1,4 +1,48 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.HotelException;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
 public class AdminController {
+    public TableColumn<User, Integer> userIdColumn;
+    public TableColumn<User, String> firstNameColumn;
+    public TableColumn<User, String> lastNameColumn;
+    public TableColumn<User, String> emailColumn;
+    public TableColumn<User, String> passwordColumn;
+    public TableColumn<User, String> usernameColumn;
+    public TableColumn<User, Boolean> adminColumn;
+    public TableView userTableID;
+    public BorderPane tablePaneID;
+    UserManager userManager=new UserManager();
+    @FXML
+    public void initialize() {
+        userIdColumn.setCellValueFactory(cellData -> {
+            User user = cellData.getValue();
+            return new SimpleIntegerProperty(user.getId()).asObject();
+        });
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("first_name"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("last_name"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<User, String>("email"));
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("username"));
+        passwordColumn.setCellValueFactory(new PropertyValueFactory<User, String>("password"));
+        adminColumn.setCellValueFactory(new PropertyValueFactory<User, Boolean>("admin"));
+        refreshUser();
+    }
+    void refreshUser(){
+        try{
+            userTableID.setItems(FXCollections.observableList(userManager.getAll()));
+            userTableID.refresh();
+        } catch (HotelException e){
+            e.printStackTrace();
+        }
+    }
 }
