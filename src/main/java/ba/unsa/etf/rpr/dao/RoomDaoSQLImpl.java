1f4bfab13e162fa83b7057eaf71c26eaba9a43b9
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-09 23:09:34 +0100

    added singleton in RoomDaoSQLImpl.java

--- src/main/java/ba/unsa/etf/rpr/dao/RoomDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RoomDaoSQLImpl.java
@@ -5,14 +5,35 @@ import ba.unsa.etf.rpr.domain.Room;
 import ba.unsa.etf.rpr.exceptions.HotelException;
 
 import java.sql.ResultSet;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * MySQL Implementation of DAO
+ * @author Dalila Krslak
+ */
 public class RoomDaoSQLImpl extends AbstractDao<Room> implements RoomDao{
-
+    private static RoomDaoSQLImpl instance = null;
     public RoomDaoSQLImpl() {
         super("ROOM");
+    }
+    /**
+     * @author Dalila Krslak
+     * @return RoomDaoSQLImpl
+     * We don't need more than one object for CRUD operations on table 'ROOM' so we will use Singleton
+     * This method will call private constructor in instance==null and then return that instance
+     */
+    public static RoomDaoSQLImpl getInstance(){
+        if(instance==null)
+            instance = new RoomDaoSQLImpl();
+        return instance;
+    }
+    /**
+     * Removes the singleton instance of the RoomDaoSQLImpl class.
+     */
+    public static void removeInstance(){
+        if(instance!=null)
+            instance=null;
     }
 
     @Override
@@ -42,13 +63,4 @@ public class RoomDaoSQLImpl extends AbstractDao<Room> implements RoomDao{
         return item;
     }
 
-    @Override
-    public Room getByDescription(String description) throws HotelException {
-        try {
-            List<Room> rooms = executeQuery("SELECT * FROM ROOM WHERE description = ?", new Object[]{description});
-            return rooms.get(0);
-        } catch (HotelException e) {
-            throw new HotelException(e.getMessage(), e);
-        }
-    }
 }
