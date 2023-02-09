a23df2720e69539b41d4de2c55cd8dcf7769f3bd
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-09 20:20:02 +0100

    implemented main in App.java

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,7 +1,13 @@
 package ba.unsa.etf.rpr;
+import ba.unsa.etf.rpr.business.ReservationsManager;
+import ba.unsa.etf.rpr.business.RoomManager;
+import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.domain.Room;
 import org.apache.commons.cli.*;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     private static final Option addRoom = new Option("add","add-room",false, "Adding new room to database");
@@ -34,6 +40,86 @@ public class App {
         Options options = addOptions();
         CommandLineParser commandLineParser = new DefaultParser();
         CommandLine cl = commandLineParser.parse(options, args);
-
+        if((cl.hasOption(addRoom.getOpt()))) {
+            try {
+                RoomManager roomManager = new RoomManager();
+                System.out.println("Input in");
+                Room room = new Room();
+                room.setDescription(cl.getArgList().get(0));
+                room.setPrice(cl.getArgList().get(1));
+                room.setStatus(Boolean.parseBoolean(cl.getArgList().get(2)));
+                room.setKapacitet(Integer.parseInt(cl.getArgList().get(3)));
+                roomManager.add(room);
+                System.out.println("You successfully added room to database!");
+            }
+            catch(Exception e){
+                System.out.println("Incorrect");
+            }
+        }
+        else if(cl.hasOption(updateRoom.getOpt())){
+            try {
+                RoomManager roomManager = new RoomManager();
+                System.out.println("Input in");
+                List<Room> list = roomManager.getAll();
+                List<Integer> ids = new ArrayList<>();
+                for(int i = 0; i < list.size(); i++){
+                    Room room= list.get(i);
+                    ids.add(room.getId());
+                }
+                if(ids.contains(Integer.valueOf(cl.getArgList().get(0)))) {
+                    Room room = new Room();
+                    room.setId(Integer.parseInt(cl.getArgList().get(0)));
+                    room.setDescription(cl.getArgList().get(1));
+                    room.setPrice(cl.getArgList().get(2));
+                    room.setStatus(Boolean.parseBoolean(cl.getArgList().get(3)));
+                    room.setKapacitet(Integer.parseInt(cl.getArgList().get(4)));
+                    roomManager.update(room);
+                    System.out.println("You successfully updated room in database!");
+                }
+                else{
+                    System.out.println("The given id doesn't exist in the database!");
+                }
+            }
+            catch(Exception e){
+                System.out.println("Incorrect");
+            }
+        }
+        else if(cl.hasOption(deleteRoom.getOpt())) {
+            try {
+                RoomManager roomManager = new RoomManager();
+                System.out.println("Input in");
+                List<Room> list = roomManager.getAll();
+                List<Integer> ids = new ArrayList<>();
+                for (int i = 0; i < list.size(); i++) {
+                    Room room = list.get(i);
+                    ids.add(room.getId());
+                }
+                if(ids.contains(Integer.valueOf(cl.getArgList().get(0)))) {
+                    roomManager.delete(Integer.parseInt(cl.getArgList().get(0)));
+                    System.out.println("You successfully deleted room from database!");
+                } else {
+                    System.out.println("The given id doesn't exist in the database!");
+                }
+            } catch (Exception e) {
+                System.out.println("Incorrect");
+            }
+        }
+        else if(cl.hasOption(getRooms.getOpt())){
+            RoomManager movieManager = new RoomManager();
+            movieManager.getAll().forEach(c -> System.out.println(c.getDescription()+ " ; " +c.getPrice()
+                    +" ; " +c.isStatus()+ " ; " +c.getKapacitet()));
+        }
+        else if(cl.hasOption(getUsers.getLongOpt())){
+            UserManager userManager = new UserManager();
+            userManager.getAll().forEach(c -> System.out.println(c.getId() + " ; " + c.getFirst_name() + " ; " + c.getLast_name() + " ; " + c.getEmail() + " ; " + c.getUsername() + " ; " + c.getPassword() + " ; " + c.isAdmin()));
+        }
+        else if(cl.hasOption(getReservations.getOpt())){
+            ReservationsManager reservationsManager = new ReservationsManager();
+            reservationsManager.getAll().forEach(c -> System.out.println(c.getId() + " ; " + c.getCheck_in() + " ; " + c.getCheck_out() + " ; " + c.getRoom_id().getDescription() + " ; " + c.getPerson_id().getUsername()));
+        }
+        else{
+            printFormattedOptions(options);
+            System.exit(-1);
+        }
     }
 }
