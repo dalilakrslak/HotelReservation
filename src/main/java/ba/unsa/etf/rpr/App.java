61b25bf5afc01ce2886ae9cb81ca42402e6a3685
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-09 19:29:18 +0100

    created App.java

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,25 +1,4 @@
 package ba.unsa.etf.rpr;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
-
-public class App extends Application
-{
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/firstPage.fxml"));
-        primaryStage.setTitle("Hello!");
-        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-    }
-    public static void main( String[] args )
-    {
-
-        launch(args);
-    }
+public class App {
 }
