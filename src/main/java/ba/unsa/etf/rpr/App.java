b868321d64571d19e036d213a816a0de417b0b11
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-09 19:33:12 +0100

    added get options in App.java

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,4 +1,8 @@
 package ba.unsa.etf.rpr;
+import org.apache.commons.cli.*;
 
 public class App {
+    private static final Option getRooms = new Option("getR", "get-rooms",false, "Printing all rooms from database");
+    private static final Option getUsers = new Option("getU", "get-users",false, "Printing all users from database");
+    private static final Option getReservations = new Option("getR", "get-reservations",false, "Printing all reservations from database");
 }
