756721f006f8f66d0f8c1fbfbe5cdb31d12a1be4
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-10 08:36:17 +0100

    updated AppFX.java for javadoc

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -7,8 +7,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
+/**
+ * class that extends Application
+ * @author Dalila Krslak
+ */
 public class AppFX extends Application
 {
+    /**
+     * Opens First Page window
+     * @param primaryStage Stage
+     * @throws Exception in case of an error
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/firstPage.fxml"));
@@ -17,9 +26,13 @@ public class AppFX extends Application
         primaryStage.setResizable(false);
         primaryStage.show();
     }
+
+    /**
+     * Main runnable method
+     * @param args
+     */
     public static void main( String[] args )
     {
-
         launch(args);
     }
 }
