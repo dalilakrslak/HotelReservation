02fe24b0118524e29a26c771069fa39c77c42c72
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-10 08:06:52 +0100

    updated HomeController.java class for javadoc

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -11,6 +11,10 @@ import java.io.IOException;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
+/**
+ * Controller for managing 'Home' window
+ * @author Dalila Krslak
+ */
 public class HomeController {
     public Button homeButtonId;
     public Button aboutUsButtonId;
@@ -18,21 +22,54 @@ public class HomeController {
     public Button contactButton;
     public Button bookButtonId;
     public BorderPane homePaneId;
+    /**
+     * Opens 'Home' window
+     * @param actionEvent ActionEvent
+     * @throws IOException in case of an error
+     */
     public void homeOnAction(ActionEvent actionEvent) throws IOException {
         openDialog("Home", "/fxml/home.fxml", new HomeController());
     }
+    /**
+     * Opens 'About Us' window
+     * @param actionEvent ActionEvent
+     * @throws IOException in case of an error
+     */
     public void aboutUsOnAction(ActionEvent actionEvent) throws IOException {
         openDialog("About us", "/fxml/aboutUs.fxml", new AboutUsController());
     }
+    /**
+     * Opens 'Rooms' window
+     * @param actionEvent ActionEvent
+     * @throws IOException in case of an error
+     */
     public void roomsOnAction(ActionEvent actionEvent) throws IOException {
         openDialog("Rooms", "/fxml/rooms.fxml", new RoomsController());
     }
+    /**
+     * Opens 'Contact' window
+     * @param actionEvent ActionEvent
+     * @throws IOException in case of an error
+     */
     public void contactOnAction(ActionEvent actionEvent) throws IOException {
         openDialog("Contact", "/fxml/contact.fxml", new ContactController());
     }
+
+    /**
+     * Opens window for booking
+     * @param actionEvent ActionEvent
+     * @throws IOException in case of an error
+     */
     public void bookNowOnAction(ActionEvent actionEvent) throws IOException {
         openDialog("Booking", "/fxml/booking.fxml", new BookingController());
     }
+    /**
+     * Opens a dialog window with the provided FXML file path
+     * @param title String for window Title
+     * @param file path of the FXML file
+     * @param controller Object
+     * @throws IOException in case of an error
+     */
     private void openDialog(String title, String file, Object controller) throws IOException {
         final Stage homeStage = (Stage) homePaneId.getScene().getWindow();
         Stage stage = new Stage();
