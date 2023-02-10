deb1d968f50039e968acc21e066c0051b28f2625
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-10 08:26:43 +0100

    updated BookingController.java class for javadoc

--- src/main/java/ba/unsa/etf/rpr/controllers/BookingController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/BookingController.java
@@ -6,17 +6,27 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 
 import java.util.ArrayList;
-
+/**
+ * Controller for form for Booking a room
+ * @author Dalila Krslak
+ */
 public class BookingController {
     public ChoiceBox<String> roomId;
     private ArrayList<String> rooms = new ArrayList<>();
 
+    /**
+     * Constructor which adds choices in ChoiceBox
+     */
     public BookingController(){
         rooms.add("Single bed");
         rooms.add("Room for couple");
         rooms.add("Two beds");
         rooms.add("Family room");
     }
+
+    /**
+     * Initializes ChoiceBox for choosing the room
+     */
     @FXML
     public void initialize(){
         roomId.setItems(FXCollections.observableList(rooms));
