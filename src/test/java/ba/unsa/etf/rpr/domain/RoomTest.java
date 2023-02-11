4e533b548bdca92970fa34f74b68f7bc46509643
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-11 15:44:59 +0100

    added testSettersAndGetters in RoomTest.java

--- src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
@@ -20,4 +20,17 @@ public class RoomTest {
         assertTrue(room.isStatus());
         assertEquals(10, room.getKapacitet());
     }
+    @Test
+    public void testSettersAndGetters() {
+        Room room = new Room();
+        room.setDescription("one-bed");
+        room.setPrice("$30");
+        room.setStatus(true);
+        room.setKapacitet(10);
+
+        assertEquals("one-bed", room.getDescription());
+        assertEquals("$30", room.getPrice());
+        assertTrue(room.isStatus());
+        assertEquals(10, room.getKapacitet());
+    }
 }
