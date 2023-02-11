47e11a4740141c4a6f0a8427eff704b2cc622f64
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-11 15:03:45 +0100

    added testUserSettersAndGetters in UserDaoTest.java

--- src/test/java/ba/unsa/etf/rpr/dao/UserDaoTest.java
+++ src/test/java/ba/unsa/etf/rpr/dao/UserDaoTest.java
@@ -1,4 +1,27 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class UserDaoTest {
+    @Test
+    public void testUserSettersAndGetters() {
+        User user = new User();
+        user.setFirst_name("Niko");
+        user.setLast_name("Nikic");
+        user.setEmail("nnikic1@etf.unsa.ba");
+        user.setUsername("nnikic1");
+        user.setPassword("topic");
+        user.setAdmin(false);
+
+        assertEquals("Niko", user.getFirst_name());
+        assertEquals("Nikic", user.getLast_name());
+        assertEquals("nnikic1@etf.unsa.ba", user.getEmail());
+        assertEquals("nnikic1", user.getUsername());
+        assertEquals("topic", user.getPassword());
+        assertFalse(user.isAdmin());
+    }
 }
