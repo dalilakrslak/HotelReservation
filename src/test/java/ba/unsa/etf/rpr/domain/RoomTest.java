004f9c97fa084c35965c5a041b558a1075d15d13
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-11 15:42:12 +0100

    added testConstructor in RoomTest.java

--- src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
@@ -1,7 +1,20 @@
 package ba.unsa.etf.rpr.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Tests for Room class
  * @author Dalila Krslak
  */
 public class RoomTest {
+    @Test
+    public void testConstructor(){
+        Room room = new Room("one-bed", "$30", true, 10);
+        assertEquals("one-bed", room.getDescription());
+        assertEquals("$30", room.getPrice());
+        assertTrue(room.isStatus());
+        assertEquals(10, room.getKapacitet());
+    }
 }
