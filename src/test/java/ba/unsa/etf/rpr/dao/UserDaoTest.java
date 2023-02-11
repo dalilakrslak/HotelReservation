273d487864a8510937691bd7bfabe6f51b641048
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-11 15:04:12 +0100

    added javadoc comment for testUserSettersAndGetters

--- src/test/java/ba/unsa/etf/rpr/dao/UserDaoTest.java
+++ src/test/java/ba/unsa/etf/rpr/dao/UserDaoTest.java
@@ -7,6 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class UserDaoTest {
+    /**
+     * tests setters and getters in UserDao
+     */
     @Test
     public void testUserSettersAndGetters() {
         User user = new User();
