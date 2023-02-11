8a8686536e5816bf43a6593ef1d284f0ad25943e
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-11 20:49:01 +0100

    added mock tests

--- src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
@@ -1,14 +1,42 @@
 package ba.unsa.etf.rpr.domain;
 
+import ba.unsa.etf.rpr.business.RoomManager;
+import ba.unsa.etf.rpr.dao.RoomDao;
+import ba.unsa.etf.rpr.exceptions.HotelException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
 /**
  * Tests for Room class
  * @author Dalila Krslak
  */
 public class RoomTest {
+    private RoomManager roomManager;
+    @Mock
+    private RoomDao roomDao;
+    public Room room  =  new Room("one-bed", "$30", true, 10);
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        roomManager = new RoomManager();
+    }
+    @Test
+    public void addTest() throws HotelException {
+        roomDao.add(room);
+        verify(roomDao).add(room);
+    }
+
+    @Test
+    void updateTest() throws Exception {
+        room.setDescription("two-bed");
+        roomDao.update(room);
+        verify(roomDao).update(room);
+    }
     /**
      * tests constructor
      */
