cfa24b7ac91a3c05acf9ae4def2e3acddc7eb7ea
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-12 10:40:31 +0100

    javadoc comments for HotelException.java

--- src/main/java/ba/unsa/etf/rpr/exceptions/HotelException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/HotelException.java
@@ -5,9 +5,19 @@ package ba.unsa.etf.rpr.exceptions;
  * @author Dalila KRslak
  */
 public class HotelException extends Exception{
+    /**
+     * Two-parameter constructor
+     * @param message String
+     * @param reason Exception
+     */
     public HotelException(String message, Exception reason){
         super(message, reason);
     }
+
+    /**
+     * One-parameter constructor
+     * @param message String
+     */
     public HotelException(String message) {
         super(message);
     }
