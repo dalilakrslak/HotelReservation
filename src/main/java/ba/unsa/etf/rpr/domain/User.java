e7314da1cb3ac5db5fb406577b562f4a90c4a8d6
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-12 10:39:04 +0100

    javadoc comments for User.java

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -14,6 +14,15 @@ public class User implements Idable {
     private String username;
     private String password;
 
+    /**
+     * Parameterized constructor
+     * @param first_name String value
+     * @param last_name String value
+     * @param email String value
+     * @param admin boolean value
+     * @param username String value
+     * @param password String value
+     */
     public User(String first_name, String last_name, String email, boolean admin, String username, String password) {
         this.first_name = first_name;
         this.last_name = last_name;
@@ -22,67 +31,116 @@ public class User implements Idable {
         this.username = username;
         this.password = password;
     }
+    /**
+     * no-arg constructor
+     */
     public User(){
 
     }
+    /**
+     * sets or updates the value of id
+     * @param id int value
+     */
     @Override
     public void setId(int id) {
         this.user_id = id;
     }
-
+    /**
+     * gets the value of id
+     * @return int value
+     */
     @Override
     public int getId() {
         return user_id;
     }
-
+    /**
+     * gets the value of id
+     * @return int value
+     */
     public String getFirst_name() {
         return first_name;
     }
-
+    /**
+     * sets or updates the value of first_name
+     * @param first_name String value
+     */
     public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
-
+    /**
+     * gets the value of id
+     * @return int value
+     */
     public String getLast_name() {
         return last_name;
     }
-
+    /**
+     * sets or updates the value of last_name
+     * @param last_name String value
+     */
     public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
-
+    /**
+     * gets the value of id
+     * @return int value
+     */
     public String getEmail() {
         return email;
     }
-
+    /**
+     * sets or updates the value of email
+     * @param email String value
+     */
     public void setEmail(String email) {
         this.email = email;
     }
-
+    /**
+     * gets the value of admin
+     * @return boolean value
+     */
     public boolean isAdmin() {
         return admin;
     }
-
+    /**
+     * sets or updates the value of admin
+     * @param admin boolean value
+     */
     public void setAdmin(boolean admin) {
         this.admin = admin;
     }
-
+    /**
+     * gets the value of username
+     * @return String value
+     */
     public String getUsername() {
         return username;
     }
-
+    /**
+     * sets or updates the value of username
+     * @param username String value
+     */
     public void setUsername(String username) {
         this.username = username;
     }
-
+    /**
+     * gets the value of password
+     * @return String value
+     */
     public String getPassword() {
         return password;
     }
-
+    /**
+     * sets or updates the value of password
+     * @param password String value
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    /**
+     * returns the String representation of the object.
+     * @return String value
+     */
     @Override
     public String toString() {
         return "User{" +
@@ -95,7 +153,11 @@ public class User implements Idable {
                 ", password='" + password + '\'' +
                 '}';
     }
-
+    /**
+     * compares two strings, and returns true if the strings are equal, and false if not
+     * @param o Object
+     * @return boolean value
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -103,7 +165,10 @@ public class User implements Idable {
         User user = (User) o;
         return user_id == user.user_id;
     }
-
+    /**
+     * generating an integer value by a hashing algorithm.
+     * @return integer value
+     */
     @Override
     public int hashCode() {
         return Objects.hash(user_id, first_name, last_name, email, admin, username, password);
