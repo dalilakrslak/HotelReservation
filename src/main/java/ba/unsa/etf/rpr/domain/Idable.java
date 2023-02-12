2252e20fa7a562029a064bebaffd36de34559ef8
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-12 10:40:21 +0100

    javadoc comments for Idable.java

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -5,8 +5,15 @@ package ba.unsa.etf.rpr.domain;
  * @author Dalila Krslak
  */
 public interface Idable {
-
+    /**
+     * sets or updates the value of id
+     * @param id int value
+     */
     void setId(int id);
 
+    /**
+     * gets the value of id
+     * @return int value
+     */
     int getId();
 }
