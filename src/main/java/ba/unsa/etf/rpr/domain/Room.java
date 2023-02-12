b0d5316152ded5051cc19a2ce5fe73a09bfd0c07
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-12 10:39:44 +0100

    javadoc comments for Room.java

--- src/main/java/ba/unsa/etf/rpr/domain/Room.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Room.java
@@ -9,43 +9,75 @@ public class Room implements Idable{
     private int room_id;
     private String description;
     private String price;
-
+    /**
+     * no-arg constructor
+     */
     public Room(){
 
     }
 
+    /**
+     * Parameterized constructor
+     * @param description String value
+     * @param price String value
+     */
     public Room(String description, String price) {
         this.description = description;
         this.price = price;
     }
-
+    /**
+     * sets or updates the value of id
+     * @param id int value
+     */
     @Override
     public void setId(int id) {
         this.room_id = id;
     }
-
+    /**
+     * gets the value of id
+     * @return int value
+     */
     @Override
     public int getId() {
         return room_id;
     }
 
+    /**
+     * gets the value of description
+     * @return String value
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * sets or updates the value of description
+     * @param description String value
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * gets the value of price
+     * @return String value
+     */
     public String getPrice() {
         return price;
     }
 
+    /**
+     * sets or updates the value of price
+     * @param price String value
+     */
     public void setPrice(String price) {
         this.price = price;
     }
 
-
+    /**
+     * returns the String representation of the object.
+     * @return String value
+     */
     @Override
     public String toString() {
         return "Room{" +
@@ -54,7 +86,11 @@ public class Room implements Idable{
                 ", price='" + price + '\'' +
                 '}';
     }
-
+    /**
+     * compares two strings, and returns true if the strings are equal, and false if not
+     * @param o Object
+     * @return boolean value
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -62,7 +98,10 @@ public class Room implements Idable{
         Room room = (Room) o;
         return room_id == room.room_id;
     }
-
+    /**
+     * generating an integer value by a hashing algorithm.
+     * @return integer value
+     */
     @Override
     public int hashCode() {
         return Objects.hash(room_id, description, price);
