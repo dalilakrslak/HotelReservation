422d732cc697e66be122ac81d98258c5581c2e62
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-12 10:39:37 +0100

    javadoc comments for RoomDao.java

--- src/main/java/ba/unsa/etf/rpr/dao/RoomDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RoomDao.java
@@ -9,5 +9,11 @@ import ba.unsa.etf.rpr.exceptions.HotelException;
  * @author Dalila Krslak
  */
 public interface RoomDao extends Dao<Room>{
+    /**
+     * Fetches Room object from table ROOM defined by description given as parameter
+     * @param description String value that represents description
+     * @return Room instance
+     * @throws HotelException in case of an error
+     */
     public Room getByDescription(String description) throws HotelException;
 }
