e2c642151256c41c214b59b90ec7291356549d7c
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-12 10:40:12 +0100

    javadoc comments for Reservations.java

--- src/main/java/ba/unsa/etf/rpr/domain/Reservations.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Reservations.java
@@ -13,59 +13,104 @@ public class Reservations implements Idable{
     private Room room_id;
     private User person_id;
 
+    /**
+     * no-arg constructor
+     */
     public Reservations(){
 
     }
 
+    /**
+     * Parameterized constructor
+     * @param check_in Date instance
+     * @param check_out Date instance
+     * @param room_id Room instance
+     * @param person_id User instance
+     */
     public Reservations(Date check_in, Date check_out, Room room_id, User person_id) {
         this.check_in = check_in;
         this.check_out = check_out;
         this.room_id = room_id;
         this.person_id = person_id;
     }
-
+    /**
+     * sets or updates the value of id
+     * @param id int value
+     */
     @Override
     public void setId(int id) {
         this.reservation_id = id;
     }
-
+    /**
+     * gets the value of id
+     * @return int value
+     */
     @Override
     public int getId() {
         return reservation_id;
     }
-
+    /**
+     * gets the value of check_in
+     * @return Date value
+     */
     public Date getCheck_in() {
         return check_in;
     }
-
+    /**
+     * sets or updates the value of check_in
+     * @param check_in Date value
+     */
     public void setCheck_in(Date check_in) {
         this.check_in = check_in;
     }
-
+    /**
+     * gets the value of check_out
+     * @return Date value
+     */
     public Date getCheck_out() {
         return check_out;
     }
 
+    /**
+     * sets or updates the value of check_out
+     * @param check_out Date value
+     */
     public void setCheck_out(Date check_out) {
         this.check_out = check_out;
     }
-
+    /**
+     * gets the value of room_id
+     * @return Room value
+     */
     public Room getRoom_id() {
         return room_id;
     }
-
+    /**
+     * sets or updates the value of room_id
+     * @param room_id Room value
+     */
     public void setRoom_id(Room room_id) {
         this.room_id = room_id;
     }
-
+    /**
+     * gets the value of person_id
+     * @return User value
+     */
     public User getPerson_id() {
         return person_id;
     }
-
+    /**
+     * sets or updates the value of person_id
+     * @param person_id User value
+     */
     public void setPerson_id(User person_id) {
         this.person_id = person_id;
     }
 
+    /**
+     * returns the String representation of the object.
+     * @return String value
+     */
     @Override
     public String toString() {
         return "Reservations{" +
@@ -77,6 +122,11 @@ public class Reservations implements Idable{
                 '}';
     }
 
+    /**
+     * compares two strings, and returns true if the strings are equal, and false if not
+     * @param o Object
+     * @return boolean value
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -85,6 +135,10 @@ public class Reservations implements Idable{
         return reservation_id == that.reservation_id;
     }
 
+    /**
+     * generating an integer value by a hashing algorithm.
+     * @return integer value
+     */
     @Override
     public int hashCode() {
         return Objects.hash(reservation_id, check_in, check_out, room_id, person_id);
