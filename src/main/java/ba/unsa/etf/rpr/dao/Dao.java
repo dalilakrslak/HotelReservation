919a810796d3b59fa8061b0f2b29f95671896aad
dalilakrslak <dkrslak1@etf.unsa.ba>
2023-02-13 14:32:40 +0100

    small changes in Dao.java

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 /**
  * Root interface for all DAO classes
- *
+ * @param <T>
  * @author Dalila Krslak
  */
 public interface Dao<T> {
@@ -21,6 +21,7 @@ public interface Dao<T> {
     /**
      * Saves entity into database
      * @param item bean for saving to database
+     * @throws HotelException in case of an error
      * @return saved item with id field populated
      */
     T add(T item) throws HotelException;
@@ -28,6 +29,7 @@ public interface Dao<T> {
     /**
      * Fully updates entity in database based on id (primary) match.
      * @param item - bean to be updated. id must be populated
+     * @throws HotelException in case of an error
      * @return updated version of bean
      */
     T update(T item) throws HotelException;
@@ -35,11 +37,13 @@ public interface Dao<T> {
     /**
      * Hard delete of item from database with given id
      * @param id - primary key of entity
+     * @throws HotelException in case of an error
      */
     void delete(int id) throws HotelException;
 
     /**
      * Lists all entities from database. WARNING: Very slow operation because it reads all records.
+     * @throws HotelException in case of an error
      * @return List of entities from database
      */
     List<T> getAll() throws HotelException;
